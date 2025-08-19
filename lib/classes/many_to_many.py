9104d2d4a1627b0ccd4c493172975e9b0dc49f9b
CodexLante <allomwas@gmail.com>
2025-08-19 15:09:57 +0300

    test

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,100 @@
+# lib/classes/many_to_many.py
+
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise ValueError("Title must be a string between 5 and 50 characters.")
+        self._title = title
+
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        # Ignore attempts to change title (immutable)
+        pass
+
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Author name must be a non-empty string.")
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # Ignore attempts to change author name (immutable)
+        pass
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        mags = list({article.magazine for article in self.articles()})
+        return mags if mags else None
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        areas = list({magazine.category for magazine in self.magazines() or []})
+        return areas if areas else None
+
 
 class Magazine:
     def __init__(self, name, category):
+        self._name = None
+        self._category = None
         self.name = name
         self.category = category
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and (2 <= len(value) <= 16):
+            self._name = value
+        # if invalid, ignore and keep old value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
+        # if invalid, ignore and keep old value
+
     def articles(self):
-        pass
+        arts = [article for article in Article.all if article.magazine == self]
+        return arts if arts else None
 
     def contributors(self):
-        pass
+        contribs = list({article.author for article in Article.all if article.magazine == self})
+        return contribs if contribs else None
 
     def article_titles(self):
-        pass
+        titles = [article.title for article in Article.all if article.magazine == self]
+        return titles if titles else None
 
     def contributing_authors(self):
-        pass
+        contribs = []
+        for author in {article.author for article in Article.all if article.magazine == self}:
+            if sum(1 for a in Article.all if a.magazine == self and a.author == author) > 2:
+                contribs.append(author)
+        return contribs if contribs else None
